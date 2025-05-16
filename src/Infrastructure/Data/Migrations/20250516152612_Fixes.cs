40b9250bca13c58de7d5c73e33b2c04bcfa525d8
xsainteer <makmatov123@gmail.com>
2025-05-16 22:31:17 +0600

    Poll creation works now

---
+++ src/Infrastructure/Data/Migrations/20250516152612_Fixes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "PollOptions",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "Polls",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PollOptions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PollOptions");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "PollOptions",
+                newName: "Text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "Polls",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
